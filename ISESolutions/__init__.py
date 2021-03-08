82b1c5397d16284b0a6d41073cce8c5d05da1f29
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-03-08 15:11:22 +0300

    Create ISESolutions/__init__.py

---
+++ ISESolutions/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.9
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.1"
+__name__ = "ISESolutions"
+
+from . import ЕГЭ
+
+__all__ = (
+    "__version__",
+    "__name__",
+    "ЕГЭ",)
