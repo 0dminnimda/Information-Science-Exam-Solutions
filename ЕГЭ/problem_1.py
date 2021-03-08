fb2abd018f976769928a705ac3f48c6bd4d0af38
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-03-08 13:49:00 +0300

    Create problem_1.py

---
+++ ЕГЭ/problem_1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.9
+
+# the problem gives us a drawing of a graph
+# and a table describing the lengths of the edges of the graph
+
+# the problem breaks down into two parts:
+# 1) comparison of names from the figure and the table
+# 2) finding a certain length with some conditions
