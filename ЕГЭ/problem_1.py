b7304a2e3843e30c1496461db7da28037f23b189
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-03-08 13:51:00 +0300

    problem_1: creaate Node class

--- ЕГЭ/problem_1.py
+++ ЕГЭ/problem_1.py
@@ -6,3 +6,16 @@
 # the problem breaks down into two parts:
 # 1) comparison of names from the figure and the table
 # 2) finding a certain length with some conditions
+
+
+class Node:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.parents: list[Node] = list()
+        self.children: list[Node] = list()
+
+    def attach(self, other: Node) -> Node:
+        other.parents.append(self)
+        self.children.append(other)
+        return self
+
