9ad12f9301996138c747f5d7dc935874239aa3e9
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-03-10 23:38:55 +0300

    Update problem_1.py (I'll just commit what I changed)

--- ISESolutions/ЕГЭ/problem_1.py
+++ ISESolutions/ЕГЭ/problem_1.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import annotations
+from collections import defaultdict
 
 __all__ = (
     "Node",)
@@ -15,18 +16,98 @@ __all__ = (
 # 2) finding a certain length with some conditions
 
 
-class Node:
+class Node2:
     def __init__(self, name: str) -> None:
         self.name = name
-        self.parents: list[Node] = list()
-        self.children: list[Node] = list()
+        self.incoming_edges: list[Edge] = list()
+        self.outgoing_edges: list[Edge] = list()
 
-    def attach(self, other: Node) -> Node:
-        other.parents.append(self)
-        self.children.append(other)
+    def attach(self, other: Node, length: int = 1) -> Node:
+        edge: Edge = Edge(self, other, length)
+        other.incoming_edges.append(edge)
+        self.outgoing_edges.append(edge)
         return self
 
 
-class Edge:
-    def __init__(self, start_node: Node, end_node: Node) -> None:
+class EdgeProperties:
+    def __init__(self, length: int = None):
+        self.length: int = length
+
+    def same_relation(self):
         pass
+
+
+class Edge:
+    def __init__(self,
+                 node1: Node, node2: Node,
+                 length: int = 1) -> None:
+        self.node1: Node = node1
+        self.node2: Node = node2
+        self.length: int = length
+
+    def are_nodes_equal(self, other: Edge):
+        return (
+            (self.node1 == other.node1)
+            and (self.node2 == other.node2))
+
+    def __eq__(self, other: Edge):
+        return (
+            self.are_nodes_equal(other)
+            and (self.length == other.length))
+
+
+class Node:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.edges: dict[Node, EdgeProperties] = dict()
+
+    def attach(self, other: Node,
+               edge_props: EdgeProperties = EdgeProperties()) -> Node:
+        if self.validate_edge(other):
+            other.edges[self] = edge_props
+            self.edges[other] = edge_props
+        return self
+
+    def validate_edge(self, other: Edge) -> bool:
+        if self.edges.get(other, None) is None:
+            return True
+        return False
+
+
+named_nodes = [Node(chr(i)) for i in range(ord("а"), ord("ж")+1)]
+nodes = [Node(str(i)) for i in range(len(named_nodes))]
+
+edge_indices = [
+    [0, 1, 19], [0, 2, 18], [0, 3, 21],
+    [1, 2, 23], [1, 5, 15], [1, 6, 16],
+    [2, 3, 22], [2, 6, 11],
+    [4, 5, 10], [4, 6, 12],
+    [5, 6, 14]]
+
+for i, j, *k in edge_indices:
+    nodes[i].attach(nodes[j], *k)
+
+named_edge_indices = [
+    [0, 1], [0, 4],
+    [1, 2], [1, 4],
+    [2, 3], [2, 4], [2, 5],
+    [3, 5], [3, 6],
+    [4, 5],
+    [5, 6]]
+
+for i, j in named_edge_indices:
+    named_nodes[i].attach(named_nodes[j])
+
+defdict = defaultdict(list)
+named_defdict = defaultdict(list)
+
+for n in named_nodes:
+    named_defdict[len(n.edges)].append(n)
+    # print(n.name, len(n.edges))
+
+# print()
+
+for n in nodes:
+    named_defdict[len(n.edges)].append(n)
+    # print(n.name, len(n.edges))
+
