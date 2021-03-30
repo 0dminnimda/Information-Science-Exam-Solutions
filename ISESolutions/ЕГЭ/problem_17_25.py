ca4de57b3a65b76e2112eb7eff1d92cd9a05cf54
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-03-31 02:31:55 +0300

    Update problem_17_25.py

--- ISESolutions/ЕГЭ/problem_17_25.py
+++ ISESolutions/ЕГЭ/problem_17_25.py
@@ -42,7 +42,7 @@ print(mul)
 
 def needed_num(n):
     c = 0
-    for d in range(2, int(n**0.5 + 1)):
+    for d in range(2, int(n**0.5 + 1), 2):
         if n % d == 0:
             c += 1
 
@@ -52,7 +52,7 @@ def needed_num(n):
     return False
 
 nums = []
-for i in range(101*10**6, 102*10**6+1, 2):
+for i in range(101*10**6, 102*10**6+1):
     if needed_num(i):
         nums.append(i) #print(i)
 breakpoint()
