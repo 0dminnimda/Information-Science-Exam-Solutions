caee51618da0d14edcbe68130c375e3d99944fc0
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-03-31 02:28:07 +0300

    Create problem_8.py

---
+++ ISESolutions/ЕГЭ/problem_8.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+st = "тимофей"
+
+c = 0
+for comb in product(st, repeat=5):
+    if comb.count("т") < 1:
+        continue
+    if comb.count("й") > 1:
+        continue
+    c += 1
+print(c)
