d72b62c574428f25b831615560cc977b756671d8
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-03-31 02:30:09 +0300

    Update problem_16.py

--- ISESolutions/ЕГЭ/problem_16.py
+++ ISESolutions/ЕГЭ/problem_16.py
@@ -1,9 +1,21 @@
-def f(n):
-    if n == 1:
-        return 1
-    elif n % 2 == 0:
-        return n * f(n-1)
-    elif n > 1:
-        return n + f(n-2)
+# def f(n):
+#     if n == 1:
+#         return 1
+#     elif n % 2 == 0:
+#         return n * f(n-1)
+#     elif n > 1:
+#         return n + f(n-2)
 
-print(f(-1))
+# print(f(-1))
+
+def f(n):
+    if n == 0:
+        return 0
+    elif n > 0 and n % 2 == 0:
+        return f(n//2)
+    else:
+        return 1 + f(n-1)
+
+for i in range(10000):
+    if f(i) == 12:
+        print(i)
