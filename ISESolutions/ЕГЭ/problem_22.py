79ff5a0c3a957ea4c136bba58496abf8d8cc2a4d
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-03-31 02:29:23 +0300

    Update problem_22.py

--- ISESolutions/ЕГЭ/problem_22.py
+++ ISESolutions/ЕГЭ/problem_22.py
@@ -5,8 +5,21 @@ def f(x):
         x //= 7
     return a
 
+def f(x):
+    m = 0
+    s = 0
+    while x > 0:
+        d = x % 8
+        s += d
+        if d > m:
+            m = d
+        x //= 8
+    return m, s
+
+out = 40
+out = 5, 12
 
 for i in range(500):
-    if f(i) == 40:
+    if f(i) == out:
         print(i)
-        break
+         #break
