9ab83eaf2603c3cd1bfdd80df6d7f7c304c9df7f
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-04-12 00:13:27 +0300

    Update problem_6.py

--- ISESolutions/ЕГЭ/problem_6.py
+++ ISESolutions/ЕГЭ/problem_6.py
@@ -5,8 +5,23 @@ def g(s):
         n *= 2
     return n
 
+def g(s):
+    s = (s + 1) // 7
+    if s == 0:
+        return None
+    n = 36
+    while s < 2050:
+        s *= 2
+        n += 3
+    return n
+
+
+c = 128
+c = 66
+
+
 for i in range(1000):
-    print(i, g(i))
-    if g(i) == 128:
+    # print(i, g(i))
+    if g(i) == c:
         print(i)
-        break
+        # break
