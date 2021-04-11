1ca19a893e4f295a75bda473a257fa5ec7167a02
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-04-12 00:09:58 +0300

    Update problem_12.py

--- ISESolutions/ЕГЭ/problem_12.py
+++ ISESolutions/ЕГЭ/problem_12.py
@@ -8,9 +8,36 @@ def g(s):
     return s
 
 
-for i in product("321", repeat=20):
-    si = "".join([str(j) for j in i])
-    res = g("0" + "1"*40 + si)
-    if res.count("2") == 10:
-        if res.count("1") == 15:
-            print("3:", res.count("3"), si.count("1") + 40, si)
+def test():
+    for i in product("321", repeat=70):
+        si = "0" + "".join(i)
+        res = g(si)
+        print(res.count("1"), res.count("2"), res.count("3"),
+              si.count("1"), si.count("2"), si.count("3"))
+# test()
+
+def sol2_1():
+    for i in product("321", repeat=20):
+        si = "0" + "1"*50 + "".join(i)
+        res = g(si)
+        if res.count("2") == 10:
+            if res.count("1") == 20:
+                print(res.count("3"), si, si.count("1"))
+
+
+def sol2_2():
+    for i in product("12", repeat=60):
+        si = "0" + "3"*10 + "".join(i)
+        res = g(si)
+        if res.count("2") == 10:
+            if res.count("1") == 20:
+                print(res.count("3"), si, si.count("1"))
+# sol22()
+
+def sol1():
+    for i in product("321", repeat=20):
+        si = "".join(i)
+        res = g("0" + "1"*40 + si)
+        if res.count("2") == 10:
+            if res.count("1") == 15:
+                print("3:", res.count("3"), si.count("1") + 40, si)
