8f92a7d0dc3a7bfb4d58d4f6bc367ec1090c5a98
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-04-12 00:10:06 +0300

    Update problem_16.py

--- ISESolutions/ЕГЭ/problem_16.py
+++ ISESolutions/ЕГЭ/problem_16.py
@@ -16,6 +16,19 @@ def f(n):
     else:
         return 1 + f(n-1)
 
-for i in range(10000):
-    if f(i) == 12:
+
+def f(n):
+    if n == 0:
+        return 0
+    elif n > 0 and n % 2 == 0:
+        return f(n//2)
+    else:
+        return 1 + f(n-1)
+
+
+c = 12
+c = 11
+
+for i in range(10**4):
+    if f(i) == c:
         print(i)
