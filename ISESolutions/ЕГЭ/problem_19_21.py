d576c436c16850aadeab5023ba008caa709722e1
0dminnimda <0dminnimda@gmail.com>
2021-09-30 00:57:34 +0300

    Create problem_19_21.py

---
+++ ISESolutions/ЕГЭ/problem_19_21.py
@@ -0,0 +1,88 @@
+
+
+# def next_person(p):
+#     return 0 if p else 1
+
+
+# def make_step():
+#     while 1:
+#         x = yield
+#         yield [x[0] + 1, ]
+
+# def s(*ls):
+#     for i in zip(*ls):
+#         yield sum(*i)
+
+
+# funcs = (
+#     lambda x: x[0] += 1,
+#     lambda x: x[0] *= 3,
+#     lambda x: x[0] += 1,
+#     lambda x: x[0] += 1,
+# )
+
+
+# def next_step(x, c, lvl=0):
+#     print(next_step([x[0] + 1, x[1]], p, c, lvl=lvl),
+#     next_step([x[0] * 3, x[1]], p, c, lvl=lvl),
+#     next_step([x[0], x[1] + 1], p, c, lvl=lvl),
+#     next_step([x[0], x[1] * 3], p, c, lvl=lvl))
+
+#     return {
+#         **next_step(x[0] + 1, p),
+#         **next_step(x[0] * 3, p),
+#         **next_step(x[1] + 1, p),
+#         **next_step(x[1] * 3, p)}
+
+###################################################
+# 19
+
+# def solve_19(x, c):
+#     if c == 2:
+#         return sum(x) >= MAX
+
+#     return any(solve_19(i, c+1)
+#                for i in calc(*x))
+
+
+# MAX = 70
+# nums = []
+# for i in range(1, 64):
+#     if solve_19((6, i), 0):
+#         nums.append(i)
+# print(min(nums), nums)
+
+###################################################
+
+
+def calc(a, b):
+    return (
+        (a + 1, b),
+        (a * 3, b),
+        (a,     b + 1),
+        (a,     b * 3))
+
+
+def next_step(x, c, lvl=0):
+    indent = lvl*4*" "
+
+    if sum(x) >= MAX:
+        if c == 2:
+            return True
+        if c > 2:
+            # print(indent, x, True)
+            return True
+        if c % 2 == 0:
+            return True
+        return False
+
+    print(indent, sum(x), x, c)
+    return all(next_step(i, c+1, lvl+1)
+               for i in calc(*x))
+
+
+MAX = 70
+next_step((6, 0), 0)
+# for i in range(1, 64):
+#     if next_step([6, i], 1, 0):
+#         print(i)
