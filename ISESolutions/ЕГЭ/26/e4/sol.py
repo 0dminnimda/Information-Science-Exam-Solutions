564f4bffe6f8a55ee3785e40d3bd3bbb2832fc39
0dminnimda <0dminnimda@gmail.com>
2022-03-24 08:17:36 +0300

    Add ЕГЭ\26\e4

---
+++ ISESolutions/ЕГЭ/26/e4/sol.py
@@ -0,0 +1,23 @@
+with open(r'ISESolutions\ЕГЭ\26\e4\27887.txt') as f:
+    data_s = f.read().split("\n")
+
+size, n = map(int, data_s.pop(0).split(" "))
+data = sorted([int(i) for i in data_s])
+
+disk = []
+
+for v in data:
+    if sum(disk) + v <= size:
+        disk.append(v)
+
+disk.pop(-1)
+
+data = list(set(data))
+for prev, cur in zip(data, data[1:]):
+    if sum(disk) + cur > size:
+        disk.append(prev)
+        break
+
+print(len(disk), disk)
+print(sum(disk), size)
+print(data)
